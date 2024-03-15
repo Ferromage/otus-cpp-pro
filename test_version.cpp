67eca0920166bccc372ba94d8e7b48ca0f0492d4
Petro Vich <sh_oberstmack@mail.ru>
2024-03-15 22:55:13 +0300

    change testing library

--- test_version.cpp
+++ test_version.cpp
@@ -1,13 +1,6 @@
-#define BOOST_TEST_MODULE test_version
-
 #include "lib.h"
+#include <gtest/gtest.h>
 
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_SUITE(test_version)
-
-BOOST_AUTO_TEST_CASE(test_valid_version) {
-	BOOST_CHECK(version::get() > 0);
+TEST(BasicTest, TestVersion	) {
+	ASSERT_GT(version::get(), 0);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
