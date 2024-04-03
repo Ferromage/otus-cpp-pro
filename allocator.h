706f8a1bb5e322a19d73b34af5e7f480c1591a35
Petro Vich <sh_oberstmack@mail.ru>
2024-04-03 20:53:33 +0300

    fix allocator implementation

--- allocator.h
+++ allocator.h
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <cstring>
+#include <cassert>
+
+namespace pp {
 
 template<typename T>
 class Allocator {
@@ -15,50 +18,44 @@ public:
     Allocator(const Allocator<U>&) noexcept {}
 
     T* allocate(std::size_t n) {
+        assert(n <= SIZE);
+
+        T* addr;
         if (data_) {
-            if (allocatedCount_ == capacity_) {
-                //std::cout << "try to reallocate " << (sizeof(T) * (capacity_ * 2)) << " bytes" << std::endl;
-                auto ptr = static_cast<T*>(::operator new(sizeof(T) * (capacity_ * 2)));
-                std::memcpy(ptr, data_, sizeof(T) * capacity_);
-                ::operator delete(data_);
-                data_ = ptr;
-                capacity_ *= 2;
+            if ((allocatedCount_ + n) > SIZE) {
+                throw std::out_of_range("out_of_range");
             }
-            allocatedCount_++;
+            addr = data_ + allocatedCount_;
+            allocatedCount_ += n;
         } else {
-            allocatedCount_ = 1;
+            allocatedCount_ = n;
             deallocatedCount_ = 0;
-            capacity_ = SIZE;
-            //std::cout << "try to allocate " << (sizeof(T) * capacity_) << " bytes" << std::endl;
-            data_ = static_cast<T*>(::operator new(sizeof(T) * capacity_));
+            //std::cout << "try to allocate " << (sizeof(T) * SIZE) << " bytes" << std::endl;
+            data_ = static_cast<T*>(::operator new(sizeof(T) * SIZE));
+            addr = data_;
         }
 
-        //std::cout << "allocated " << n << " items by address: " << (data_ + (allocatedCount_ - 1)) << std::endl;
-        return data_ + (allocatedCount_ - 1);
+        //std::cout << "allocated " << n << " items" << std::endl;
+        return addr;
     }
 
-    void deallocate(T* p, std::size_t n) {
+    void deallocate(T*, std::size_t n) {
         //std::cout << "deallocate " << n << " items" << std::endl;
 
         if (data_ && allocatedCount_ > 0) {
-            if (++deallocatedCount_ == allocatedCount_) {
+            if ((deallocatedCount_ + n) >= allocatedCount_) {
                 ::operator delete(data_);
                 data_ = nullptr;
                 //std::cout << "free memory" << std::endl;
             }
+            deallocatedCount_ += n;
         }
     }
-
-    // template<typename UU>
-    // struct rebind {
-    //     typedef Allocator<UU> other;
-    // };
 
 private:
     T* data_ = nullptr;
     std::size_t allocatedCount_ = 0;
     std::size_t deallocatedCount_ = 0;
-    std::size_t capacity_ = 0;
 };
 
 template<typename T, typename U>
@@ -70,3 +67,5 @@ template<typename T, typename U>
 constexpr bool operator!= (const Allocator<T>& lhs, const Allocator<U>& rhs) noexcept {
     return lhs.data_ != rhs.data_;
 }
+
+} //namespace pp
--- main.cpp
+++ main.cpp
@@ -1,6 +1,46 @@
 #include "allocator.h"
 #include <map>
-#include <cassert>
+#include <memory>
+
+template<typename T, typename Allocator = std::allocator<T>>
+class Container {
+public:
+    Container() {}
+    
+    ~Container() {
+         std::allocator_traits<decltype(alloc_)>::destroy(alloc_, data_);
+         std::allocator_traits<decltype(alloc_)>::deallocate(alloc_, data_, CAPACITY);
+    }
+
+    void push_back(const T& item) {
+        if (!data_) {            
+            size_ = 0;
+            data_ = std::allocator_traits<decltype(alloc_)>::allocate(alloc_, CAPACITY);
+        }
+
+        std::allocator_traits<decltype(alloc_)>::construct(alloc_, data_ + size_, item);
+        size_++;
+    }
+
+    std::size_t size() const {
+        return size_;
+    }
+    
+    const T& at(int idx) const {
+        if (0 <= idx && idx < (int)size_) {
+            return data_[idx];
+        }
+        throw std::out_of_range("out_of_range");
+    }
+
+private:
+    static constexpr std::size_t CAPACITY = 10;
+
+    Allocator alloc_;
+    std::size_t size_ = 0;
+    T* data_ = nullptr;
+};
+
 
 int factorial(int num) {
     assert(num >= 0);
@@ -19,21 +59,39 @@ int factorial(int num) {
 }
 
 int main() {        
-    std::map<int, int> data1;
-    for (int i = 0; i < 10; i++) {
-        data1[i] = factorial(i);
+    {
+        std::map<int, int> data;
+        for (int i = 0; i < 10; i++) {
+            data[i] = factorial(i);
+        }
     }
 
-    std::map<int, int, std::less<int>, Allocator<std::pair<int, int>>> data2;
-    for (int i = 0; i < 10; i++) {
-        data2[i] = factorial(i);
-    }
+    {
+        std::map<int, int, std::less<int>, pp::Allocator<std::pair<int, int>>> data;
+        for (int i = 0; i < 10; i++) {
+            data[i] = factorial(i);
+        }
 
-    for (const auto& [k, v] : data2) {
-        std::cout << k << ' ' << v << std::endl;
+        for (const auto& [k, v] : data) {
+            std::cout << k << ' ' << v << std::endl;
+        }
     }
     
-    
-       
+    {
+        Container<int> data;
+        for (int i = 0; i < 10; i++) {
+            data.push_back(i);
+        }
+    }
 
+    {
+        Container<int, pp::Allocator<int>> data;
+        for (std::size_t i = 0; i < 10; i++) {
+            data.push_back(i);
+        }
+        
+        for (std::size_t i = 0; i < data.size(); i++) {
+            std::cout << data.at(i) << std::endl;
+        }
+    }
 }
