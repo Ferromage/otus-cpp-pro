1525bcb0d7bd9bddd75c4924330f80c94ff0803f
Ferromage <Ferromage@users.noreply.github.com>
2024-04-27 19:54:44 +0000

    deploy: 2b21842230d7b76ddf2e9542127498fa3fa1c36f

---
+++ search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['value_0',['value',['../structall__same__type.html#ae75b13d1950bbd929c62e9aadb9950eb',1,'all_same_type']]]
+];
---
+++ search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['print_5fip_0',['print_ip',['../main_8cpp.html#ac550f70223fe6874a01760f1c8b968a4',1,'print_ip(T t):&#160;main.cpp'],['../main_8cpp.html#ae6f4cdf35193f957ccc1fea7183998d3',1,'print_ip(const T &amp;t):&#160;main.cpp'],['../main_8cpp.html#a33387371e2df89bf9e7cda945e68177a',1,'print_ip(const T &amp;t):&#160;main.cpp']]],
+  ['printtuple_1',['printTuple',['../main_8cpp.html#a616dd10c749f74f5e9bcb3077a72733a',1,'main.cpp']]]
+];
