fa7bb3b843f15594ccc8d40df7b3daac8e24e075
Petro Vich <sh_oberstmack@mail.ru>
2024-05-21 21:09:04 +0300

    add proxy

--- main.cpp
+++ main.cpp
@@ -28,45 +28,25 @@ public:
 		std::map<int, T> columnToValue_;
 	};
 
-	Matrix()  = default;
-
-	std::size_t size() {
-		adjustSize();
-		return size_;
-	}
-
-	const Line& operator[] (int row) const {
-		if (auto it = data_.find(row); it != data_.end()) {
-			return it->second;
-		}
-		return emptyItem_;
-	}
-
-	Line& operator[] (int row) {
-		isSizeMaybeChanged = true;
-		return data_[row];
-	}
-
-	
 	struct Iterator {
 		Iterator(bool isBegin, std::map<int, Line>& data) : data_(data) {
 			if (isBegin) {
-				mainIter_ = data_.begin();
-				if (mainIter_ != data_.end()) {
-					auxIter_ = mainIter_->second.columnToValue_.begin();
+				rowIter_ = data_.begin();
+				if (rowIter_ != data_.end()) {
+					columnIter_ = rowIter_->second.columnToValue_.begin();
 				}
 			} else {
-				mainIter_ = data_.end();
+				rowIter_ = data_.end();
 			}
 		}
 
 		Iterator& operator++ () {
-			if (mainIter_ != data_.end() && auxIter_ != mainIter_->second.columnToValue_.end()) {
-				++auxIter_;
-				if (auxIter_ == mainIter_->second.columnToValue_.end()) {
-					++mainIter_;
-					if (mainIter_ != data_.end()) {
-						auxIter_ = mainIter_->second.columnToValue_.begin();
+			if (rowIter_ != data_.end() && columnIter_ != rowIter_->second.columnToValue_.end()) {
+				++columnIter_;
+				if (columnIter_ == rowIter_->second.columnToValue_.end()) {
+					++rowIter_;
+					if (rowIter_ != data_.end()) {
+						columnIter_ = rowIter_->second.columnToValue_.begin();
 					}
 				}
 			}
@@ -74,39 +54,98 @@ public:
 		}
 
 		std::tuple<int, int, T> operator* () {
-			return std::make_tuple(mainIter_->first, auxIter_->first, auxIter_->second);
+			return std::make_tuple(rowIter_->first, columnIter_->first, columnIter_->second);
 		}
 
 		bool operator!= (const Iterator& rhs) const {
-			return mainIter_ != rhs.mainIter_ &&
-				   auxIter_ != rhs.auxIter_;
+			return rowIter_ != rhs.rowIter_ &&
+				   columnIter_ != rhs.columnIter_;
 		}
 
 		std::map<int, Line>& data_;
-		typename std::map<int, Line>::iterator mainIter_;
-		typename std::map<int, T>::iterator auxIter_;
+		typename std::map<int, Line>::iterator rowIter_;
+		typename std::map<int, T>::iterator columnIter_;
 	};
 
+	Matrix() = default;
+
+	std::size_t size() {
+		calcSize();
+		return size_;
+	}
+
+	const Line& operator[] (int row) const {
+		if (auto it = data_.find(row); it != data_.end()) {
+			return it->second;
+		}
+		return emptyLine_;
+	}
+
+	Line& operator[] (int row) {
+		return data_[row];
+	}
+
 	Iterator begin() {
-		isSizeMaybeChanged = true;
-		adjustSize();
 		return Iterator(true, data_);
 	}
 
 	Iterator end() {
-		isSizeMaybeChanged = true;
-		adjustSize();
 		return Iterator(false, data_);
+	}
+
+private:
+	void calcSize() {
+		size_ = 0;
+		for (auto it = data_.begin(); it != data_.end(); ++it) {
+			for (auto itt = it->second.columnToValue_.begin(); itt != it->second.columnToValue_.end(); ++itt) {
+				size_++;
+			}
+		}
+	}
+
+	template<typename U, U> friend class MatrixProxy;
+
+	mutable std::map<int, Line> data_; //row index to line
+	std::size_t size_ = 0;
+	const Line emptyLine_;
+};
+
+
+template<typename T, T DefaultValue>
+class MatrixProxy {
+public:
+	MatrixProxy(Matrix<T, DefaultValue>& matrix) : matrix_(matrix) {}
+
+	std::size_t size() {
+		adjustSize();
+		return matrix_.size();
+	}
+
+	const typename Matrix<T, DefaultValue>::Line& operator[] (int row) const {
+		return matrix_.operator[](row);
+	}
+
+	typename Matrix<T, DefaultValue>::Line& operator[] (int row) {
+		isSizeMaybeChanged = true;
+		return matrix_.operator[](row);
+	}
+
+	typename Matrix<T, DefaultValue>::Iterator begin() {
+		adjustSize();
+		return matrix_.begin();
+	}
+
+	typename Matrix<T, DefaultValue>::Iterator end() {
+		adjustSize();
+		return matrix_.end();
 	}
 
 private:
 	void adjustSize() {
 		if (isSizeMaybeChanged) {
-			size_ = 0;
-			for (auto it = data_.begin(); it != data_.end(); ) {
+			for (auto it = matrix_.data_.begin(); it != matrix_.data_.end(); ) {
 				for (auto itt = it->second.columnToValue_.begin(); itt != it->second.columnToValue_.end(); ) {
 					if (itt->second != DefaultValue) {
-						size_++;
 						++itt;
 					} else {
 						itt = it->second.columnToValue_.erase(itt);
@@ -114,7 +153,7 @@ private:
 				}
 
 				if (it->second.columnToValue_.empty()) {
-					it = data_.erase(it);
+					it = matrix_.data_.erase(it);
 				} else {
 					++it;
 				}
@@ -124,46 +163,62 @@ private:
 		}
 	}
 
-	mutable std::map<int, Line> data_; //first == row, second == map <column, value>
-	mutable std::size_t size_ = 0;
-	mutable bool isSizeMaybeChanged = false;
-	const Line emptyItem_;
+	Matrix<T, DefaultValue>& matrix_;
+	bool isSizeMaybeChanged = false;
 };
+
 
 int main() {
 	{
 		Matrix<int, -1> matrix;
-		assert(matrix.size() == 0); // все ячейки свободны
+		assert(matrix.size() == 0);
 		auto a = matrix[0][0];
 		assert(a == -1);
-		assert(matrix.size() == 0);
+		assert(matrix.size() == 1);
 		matrix[100][100] = 314;
 		assert(matrix[100][100] == 314);
-		assert(matrix.size() == 1);
+		assert(matrix.size() == 2);
 		matrix[100][100] = -1;
 		assert(matrix[100][100] == -1);
-		assert(matrix.size() == 0);
+		assert(matrix.size() == 2);
 	}
 
 	{
+		Matrix<int, -2> matrix;
+		MatrixProxy proxy(matrix);
+		assert(proxy.size() == 0); // все ячейки свободны
+		auto a = proxy[0][0];
+		assert(a == -2);
+		assert(proxy.size() == 0);
+		proxy[100][100] = 314;
+		assert(proxy[100][100] == 314);
+		assert(proxy.size() == 1);
+		proxy[100][100] = -2;
+		assert(proxy[100][100] == -2);
+		assert(proxy.size() == 0);
+	}
+	
+	{
 		Matrix<int, 0> matrix;
+		MatrixProxy proxy(matrix);
+
 		for (int i = 0; i < 10; i++) {
-			matrix[i][i] = i;
+			proxy[i][i] = i;
 		}
 		for (int i = 0; i < 10; i++) {
-			matrix[i][9 - i] = 9 - i;
+			proxy[i][9 - i] = 9 - i;
 		}
 
 		for (int r = 1; r < 9; r++) {
 			for (int c = 1; c < 9; c++) {
-				std::cout << matrix[r][c] << ' ';
+				std::cout << proxy[r][c] << ' ';
 			}
 			std::cout << "\b " << std::endl;
 		}
 
-		std::cout << matrix.size() << std::endl;
+		std::cout << proxy.size() << std::endl;
 
-		for (const auto& [x, y, value] : matrix) {
+		for (const auto& [x, y, value] : proxy) {
 			std::cout << "(" << x << "," << y << "): " << value << std::endl;
 		}
 	}
